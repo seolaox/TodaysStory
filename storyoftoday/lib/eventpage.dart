c8ab93ee60f6a2d41b9f394c6b44307f9b85c6c8
SEOLA <seolaox89@gmail.com>
2024-02-07 18:31:44 +0900

    오버플로우 수정

--- storyoftoday/lib/eventpage.dart
+++ storyoftoday/lib/eventpage.dart
@@ -52,181 +52,179 @@ class _EventPageState extends State<EventPage> {
                 } else {
                   return Column(
                     children: [
-                      SizedBox(
-                        height: 430,
-                        child: TableCalendar(
-                          daysOfWeekHeight: 50,
-                          firstDay: DateTime.utc(2000, 1, 1),
-                          lastDay: DateTime.utc(2099, 12, 31),
-                          locale: "ko_KR",
-                          headerStyle: const HeaderStyle(
-                            formatButtonVisible: false,
-                            titleCentered: true,
-                          ),
-                          focusedDay: focusedDay,
-                          onDaySelected:
-                              (DateTime selectedDay, DateTime focusedDay) {
-                            setState(() {
-                              this.selectedDay = selectedDay;
-                              this.focusedDay = focusedDay;
-                              events = {};
-                            });
-                          },
-                          eventLoader: (day) {
-                            return events[day] ?? [];
-                          },
-                          calendarBuilders: CalendarBuilders(
-                            markerBuilder: (context, day, events) {
-                              final eventCount = events.length;
-                              if (eventCount > 0) {
-                                return Positioned(
-                                  bottom: 0,
-                                  child: Row(
-                                    children: List.generate(
-                                      eventCount,
-                                      (index) => Container(
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Color.fromARGB(
-                                              255, 172, 148, 242),
-                                        ),
-                                        width: 10,
-                                        height: 10,
+                      TableCalendar(
+                        daysOfWeekHeight: 20,
+                        firstDay: DateTime.utc(2000, 1, 1),
+                        lastDay: DateTime.utc(2099, 12, 31),
+                        locale: "ko_KR",
+                        headerStyle: const HeaderStyle(
+                          formatButtonVisible: false,
+                          titleCentered: true,
+                        ),
+                        focusedDay: focusedDay,
+                        onDaySelected:
+                            (DateTime selectedDay, DateTime focusedDay) {
+                          setState(() {
+                            this.selectedDay = selectedDay;
+                            this.focusedDay = focusedDay;
+                            events = {};
+                          });
+                        },
+                        eventLoader: (day) {
+                          return events[day] ?? [];
+                        },
+                        calendarBuilders: CalendarBuilders(
+                          markerBuilder: (context, day, events) {
+                            final eventCount = events.length;
+                            if (eventCount > 0) {
+                              return Positioned(
+                                bottom: 0,
+                                child: Row(
+                                  children: List.generate(
+                                    eventCount,
+                                    (index) => Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Color.fromARGB(
+                                            255, 172, 148, 242),
                                       ),
+                                      width: 10,
+                                      height: 10,
                                     ),
                                   ),
-                                );
-                              } else {
-                                return null;
-                              }
-                            },
-                          ),
-                          selectedDayPredicate: (DateTime day) {
-                            return isSameDay(
-                                selectedDay ?? DateTime.now(), day);
+                                ),
+                              );
+                            } else {
+                              return null;
+                            }
                           },
                         ),
+                        selectedDayPredicate: (DateTime day) {
+                          return isSameDay(
+                              selectedDay ?? DateTime.now(), day);
+                        },
                       ),
-                      const SizedBox(
-                        height: 0),
+                      SizedBox(height: 15,),
                       if (events[selectedDay] != null)
-                        Container(
-                          padding: const EdgeInsets.only(top: 30),
-                          height: MediaQuery.of(context).size.height * 0.33,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(50),
-                              topRight: Radius.circular(50),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.only(top: 20),
+                            // height: MediaQuery.of(context).size.height * 0.178,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(50),
+                                topRight: Radius.circular(50),
+                              ),
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .secondaryContainer,
                             ),
-                            color: Theme.of(context)
-                                .colorScheme
-                                .secondaryContainer,
-                          ),
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                top: -5,
-                                left: 16,
-                                child: Text(
-                                  '- 이날의 이야기 -',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary,
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  top: -5,
+                                  left: 16,
+                                  child: Text(
+                                    '- 이날의 이야기 -',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Positioned(
-                                top: 25,
-                                left: 0,
-                                right: 0,
-                                bottom: 0,
-                                child: ListView.builder(
-                                  itemCount: events[selectedDay]?.length ?? 0,
-                                  itemBuilder: (context, index) {
-                                    MyEvent event =
-                                        events[selectedDay]![index];
-                                    return Card(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: SizedBox(
-                                          height: 130,
-                                          width: double.infinity,
-                                          child: Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              SizedBox(
-                                                width:  130,
-                                                height: 100,
-                                                // height: MediaQuery.of(context).size.height * 0.001,
-                                                child: (event.image != null)
-                                                    ? Image.memory(
-                                                        event.image!,
-                                                        fit: BoxFit.cover,
+                                Positioned(
+                                  top: 25,
+                                  left: 0,
+                                  right: 0,
+                                  bottom: 0,
+                                  child: ListView.builder(
+                                    itemCount: events[selectedDay]?.length ?? 0,
+                                    itemBuilder: (context, index) {
+                                      MyEvent event =
+                                          events[selectedDay]![index];
+                                      return Card(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: SizedBox(
+                                            height: 130,
+                                            width: double.infinity,
+                                            child: Row(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                SizedBox(
+                                                  width:  130,
+                                                  height: 100,
+                                                  // height: MediaQuery.of(context).size.height * 0.001,
+                                                  child: (event.image != null)
+                                                      ? Image.memory(
+                                                          event.image!,
+                                                          fit: BoxFit.cover,
+                                                        )
+                                                      : Container(
+                                                      width: double.infinity,
+                                                      color:(event.image != null) ? Color.fromARGB(255, 222, 228, 246) : Colors.transparent,
+                                                      child: (event.image != null)
+                                                          ?  Image.memory(event.image!,fit: BoxFit.cover,)
+                                                          : Icon(
+                                                        Icons.event_busy,
+                                                        size: 48,
+                                                        color: Color.fromARGB(255, 144, 144, 170),
                                                       )
-                                                    : Container(
-                                                    width: double.infinity,
-                                                    color:(event.image != null) ? Color.fromARGB(255, 222, 228, 246) : Colors.transparent,
-                                                    child: (event.image != null)
-                                                        ?  Image.memory(event.image!,fit: BoxFit.cover,)
-                                                        : Icon(
-                                                      Icons.event_busy,
-                                                      size: 48,
-                                                      color: Color.fromARGB(255, 144, 144, 170),
                                                     )
-                                                  )
-                                              ),
-                                              const SizedBox(width: 20),
-                                              Flexible(
-                                                child: Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment
-                                                          .start,
-                                                  children: [
-                                                    Row(
-                                                      children: [
-                                                        Text(
-                                                          event.title,
-                                                          style:
-                                                              const TextStyle(
-                                                            fontSize: 14,
-                                                          ),
-                                                          maxLines: 1,
-                                                          overflow: TextOverflow.ellipsis,
-                                                        ),
-                                                        IconButton(
-                                                          onPressed: () {},
-                                                          iconSize: 16,
-                                                          icon: getIconWidget(
-                                                              event.weathericon ??
-                                                                  ''),
-                                                        ),
-                                                      ],
-                                                    ),
-                                                    Text(
-                                                      event.eventdate,
-                                                      style: const TextStyle(
-                                                        fontSize: 15,
-                                                      ),
-                                                      maxLines: 3,
-                                                      overflow: TextOverflow
-                                                          .ellipsis,
-                                                    ),
-                                                  ],
                                                 ),
-                                              ),
-                                            ],
+                                                const SizedBox(width: 20),
+                                                Flexible(
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Row(
+                                                        children: [
+                                                          Text(
+                                                            event.title,
+                                                            style:
+                                                                const TextStyle(
+                                                              fontSize: 14,
+                                                            ),
+                                                            maxLines: 1,
+                                                            overflow: TextOverflow.ellipsis,
+                                                          ),
+                                                          IconButton(
+                                                            onPressed: () {},
+                                                            iconSize: 16,
+                                                            icon: getIconWidget(
+                                                                event.weathericon ??
+                                                                    ''),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                      Text(
+                                                        event.eventdate,
+                                                        style: const TextStyle(
+                                                          fontSize: 15,
+                                                        ),
+                                                        maxLines: 3,
+                                                        overflow: TextOverflow
+                                                            .ellipsis,
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    );
-                                  },
+                                      );
+                                    },
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                     ],
